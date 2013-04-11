52062e1f933b68137793be6df7092159c42c8722
Alexander Murashov <rodger@ladela.com>
2013-04-11 16:07:15 +0300

    fix extension class

--- DependencyInjection/RodgermdStravaClientExtension.php
+++ DependencyInjection/RodgermdStravaClientExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class StravaClientExtension extends Extension
+class RodgermdStravaClientExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -20,7 +20,6 @@ class StravaClientExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
